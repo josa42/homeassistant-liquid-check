2eaa756d8cfa4c0538bc232dff52497f453d3ae7
Josa Gesell <josa@gesell.me>
2025-10-22 23:22:06 +0200

    feat: improve config flow labels and allow scan interval of 0

--- custom_components/liquid_check/config_flow.py
+++ custom_components/liquid_check/config_flow.py
@@ -15,7 +15,7 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
         vol.Required("name"): str,
         vol.Required("host"): str,
         vol.Optional("scan_interval", default=60): vol.All(
-            vol.Coerce(int), vol.Range(min=10, max=3600)
+            vol.Coerce(int), vol.Range(min=0, max=3600)
         ),
     }
 )
